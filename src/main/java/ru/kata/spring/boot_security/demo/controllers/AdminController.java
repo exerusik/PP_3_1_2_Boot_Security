7f3a6969622276ac5bce74eb1bd7535fca7ee569
exerusik <exe.rusik@gmail.com>
2024-05-07 11:41:33 +0300

    rest controllers

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -1,62 +1,27 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import ru.kata.spring.boot_security.demo.model.Role;
+import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.userService.RoleService;
 import ru.kata.spring.boot_security.demo.userService.UserService;
 
-import java.security.Principal;
 import java.util.List;
-
 
 @Controller
 public class AdminController {
-    private final UserService userService;
-    private final RoleService roleService;
-
-    @Autowired
-    public AdminController(UserService userService, RoleService roleService) {
-        this.userService = userService;
-        this.roleService = roleService;
-    }
-
 
     @GetMapping("/admin")
-    public String showAll(Model model, Principal principal) {
-        List<User> users = userService.getAll();
-        User authUser = userService.findUserByUsername(principal.getName());
-        List<Role> roles = roleService.getRoles();
-        model.addAttribute("roles", roles);
-        model.addAttribute("authUser", authUser);
-        model.addAttribute("users", users);
+    public String showAll() {
         return "showAll";
     }
 
-
-    @PostMapping("/admin/new")
-    public String saveUser(@ModelAttribute User user) {
-        userService.saveUser(user);
-        return "redirect:/admin";
-    }
-
-    @PostMapping("/admin/delete")
-    public String delete(@RequestParam(name = "id") long id) {
-        userService.deleteUserById(id);
-        return "redirect:/admin";
+    @GetMapping("/user")
+    public String showUser() {
+        return "user";
     }
 
 
-    @PostMapping("/admin/userEdit")
-    public String editUser(@ModelAttribute User user) {
-        userService.editUser(user);
-
-        return "redirect:/admin";
-    }
 }
---
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminRestController.java
@@ -0,0 +1,54 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.userService.RoleService;
+import ru.kata.spring.boot_security.demo.userService.UserService;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/restAdmin")
+public class AdminRestController {
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @Autowired
+    public AdminRestController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping()
+    public List<User> showAll() {
+        return userService.getAll();
+//        User authUser = userService.findUserByUsername(principal.getName());
+//        List<Role> roles = roleService.getRoles();
+//        model.addAttribute("roles", roles);
+//        model.addAttribute("authUser", authUser);
+//        model.addAttribute("users", users);
+//        return "showAll";
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveUser(@RequestBody User user) {
+         userService.saveUser(user);
+    }
+
+    @DeleteMapping()
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@RequestParam(name = "id") long id) {
+        userService.deleteUserById(id);
+
+    }
+
+
+    @PutMapping()
+    public void editUser(@RequestBody User user) {
+        userService.editUser(user);
+    }
+}
