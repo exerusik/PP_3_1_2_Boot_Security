1b5bf74ed31bc4c3314dfa1c06d51b0a6568c4b2
exerusik <exe.rusik@gmail.com>
2024-05-10 14:49:53 +0300

    rewrite view "user" via js

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -22,6 +22,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         this.passwordEncoder = passwordEncoder;
     }
 
+
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserRestController.java
@@ -1,35 +1,35 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.kata.spring.boot_security.demo.model.Role;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.userService.RoleService;
 import ru.kata.spring.boot_security.demo.userService.UserService;
 
 import java.security.Principal;
-import java.util.List;
 
-@Controller
-public class UserController {
+@RestController
+@RequestMapping("/restUser")
+public class UserRestController {
 
     private final UserService userService;
     private final RoleService roleService;
 
     @Autowired
-    public UserController(UserService userService, RoleService roleService) {
+    public UserRestController(UserService userService, RoleService roleService) {
         this.userService = userService;
         this.roleService = roleService;
     }
 
-    @GetMapping("/user")
-    public String showUser(Principal principal, Model model) {
-        User user = userService.findUserByUsername(principal.getName());
-        model.addAttribute("user", user);
-        List<Role> roles = user.getRoles();
-        model.addAttribute("roles", roles);
-        return "user";
+    @GetMapping()
+    public User showUser(Principal principal) {
+        return userService.findUserByUsername(principal.getName());
+//        model.addAttribute("user", user);
+//        List<Role> roles = user.getRoles();
+//        model.addAttribute("roles", roles);
+//        return "user";
     }
 }
